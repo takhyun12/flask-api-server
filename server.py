66fdf0f3ffa3eccce5c75c217d29d53f883d6cdf
jyoonab <jyoona12345@gmail.com>
2021-07-12 17:55:12 +0900

    user information request function finished

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 __version__ = '1.0.0'
 
-from flask import Flask, request
+from flask import Flask
 from flask_restx import Api, Resource
 import json
 
@@ -15,15 +15,25 @@ class Api_Gateway(Resource):
     def get():
         try:
             with open('./data/serial_data.json', encoding='UTF8') as serial_json:
-                user_id: str = request.args.get('user', type = str)
+                serial_data: json = json.load(serial_json)
+                return serial_data
+        except FileNotFoundError:
+            return None
+
+@api.route('/user/<string:user_id>')
+class User_Information(Resource):
+    @staticmethod
+    def get(user_id):
+        try:
+            with open('./data/serial_data.json', encoding='UTF8') as serial_json:
                 serial_data: json = json.load(serial_json)
                 user_info: list = serial_data["users"]
 
                 for i in range(len(user_info)):
                     if user_info[i]["user_id"] == user_id:
                         return user_info[i]
-
                 return None
+
         except FileNotFoundError:
             return None
 
