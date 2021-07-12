94eaf57b5b720bead0ecd2f361649bdca00948ff
takhyun12 <takhyun12@gmail.com>
2021-07-12 18:20:07 +0900

    client code 버전 포함

--- server.py
+++ server.py
@@ -1,24 +1,22 @@
 __version__ = '1.0.0'
 
-from flask import Flask
+from flask import Flask, request
 from flask_restx import Api, Resource
 import json
 
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, version='1.0.0')
+
+json_data = open('./data/serial_data.json', encoding='UTF8')
+serial_data: dict = json.load(json_data)
 
 
-@api.route('/serial')
-class Api_Gateway(Resource):
+@api.route('/serial/<string:user_id>')
+class Serial(Resource):
     @staticmethod
-    def get():
-        try:
-            with open('./data/serial_data.json', encoding='UTF8') as serial_json:
-                serial_data: json = json.load(serial_json)
-                return serial_data
-        except FileNotFoundError:
-            return None
+    def get(user_id):
+        return [data for data in serial_data['data'] if data['user_id'] == user_id]
 
 
 if __name__ == "__main__":
